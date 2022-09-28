17379ba46918a9f11aecd765e94ffa6da5f148bd
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-28 12:43:25 +0200

    gomb working

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './style.css';
 
+function init(){
+    console.log('Loaded');    
+}
+
+function sotetfeher() {    
+        let element = document.body;
+        element.classList.toggle("dark-mode");    
+}
+
+document.getElementById('gomb').addEventListener('click',sotetfeher);
+document.addEventListener('DOMContentLoaded',init);
+console.log('betöltött');
+
