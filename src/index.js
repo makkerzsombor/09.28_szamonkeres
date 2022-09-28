bb3ec5323e73796a92d10cc1090fcdb9b84a1630
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-28 13:33:39 +0200

    birdercolor not working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import './style.css';
 
-
-function init(){
-    console.log('Loaded');    
-}
-
 function sotetfeher() {    
         let element = document.body;
         element.classList.toggle("dark-mode");    
@@ -14,14 +9,13 @@ function urlChange() {
     document.getElementById('kepid').src = document.getElementById('kepurl').value;
 }
 function widthChange() {
-    console.log('képszélessége');
     document.getElementById('kepid').style.width = parseInt(document.getElementById('kepszel').value)+"px";
 }
 function kepKeretChange() {        
         document.getElementById('kepid').style.border = parseInt(document.getElementById('kepkervas').value) +"px solid"+ document.getElementById('kepkerszin').value
 }
-function borderChange(e) {    
-    document.getElementById('kepid').style.borderImageColor = e.CurrenTarget().value;
+function borderChange() {    
+    document.getElementById('kepid').style.borderImageColor = document.getElementById('kepkerszin').value;
 }
 
 document.getElementById('kepurl').addEventListener('change',urlChange)
@@ -29,5 +23,3 @@ document.getElementById('kepszel').addEventListener('change',widthChange)
 document.getElementById('kepkervas').addEventListener('change',kepKeretChange)
 document.getElementById('kepkerszin').addEventListener('change',borderChange)
 document.getElementById('gomb').addEventListener('click',sotetfeher);
-document.addEventListener('DOMContentLoaded',init);
-console.log('betöltött');
