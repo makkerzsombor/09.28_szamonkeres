13ae7ebff2498d905b384ac961eb3bfaae094059
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-28 13:30:22 +0200

    almost done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 
+
 function init(){
     console.log('Loaded');    
 }
@@ -9,7 +10,24 @@ function sotetfeher() {
         element.classList.toggle("dark-mode");    
 }
 
+function urlChange() {
+    document.getElementById('kepid').src = document.getElementById('kepurl').value;
+}
+function widthChange() {
+    console.log('képszélessége');
+    document.getElementById('kepid').style.width = parseInt(document.getElementById('kepszel').value)+"px";
+}
+function kepKeretChange() {        
+        document.getElementById('kepid').style.border = parseInt(document.getElementById('kepkervas').value) +"px solid"+ document.getElementById('kepkerszin').value
+}
+function borderChange(e) {    
+    document.getElementById('kepid').style.borderImageColor = e.CurrenTarget().value;
+}
+
+document.getElementById('kepurl').addEventListener('change',urlChange)
+document.getElementById('kepszel').addEventListener('change',widthChange)
+document.getElementById('kepkervas').addEventListener('change',kepKeretChange)
+document.getElementById('kepkerszin').addEventListener('change',borderChange)
 document.getElementById('gomb').addEventListener('click',sotetfeher);
 document.addEventListener('DOMContentLoaded',init);
 console.log('betöltött');
-
